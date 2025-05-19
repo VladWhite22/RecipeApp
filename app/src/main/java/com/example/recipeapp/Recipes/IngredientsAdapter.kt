41d02473d80fc1dd53e671a8e1859519f6938a78
VladWhite22 <vladislav22971@mail.ru>
2025-05-20 01:26:24 +0300

    RA-14 v.0 Добавить SeekBar для расчета ингредиентов

--- app/src/main/java/com/example/recipeapp/Recipes/IngredientsAdapter.kt
+++ app/src/main/java/com/example/recipeapp/Recipes/IngredientsAdapter.kt
@@ -7,30 +7,43 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.aseducationalproject.Domain.Ingredient
 import com.example.recipeapp.databinding.ItemIngredientBinding
 
-class IngredientsAdapter(private val dataSet: List<Ingredient>):
+class IngredientsAdapter(private val dataSet: List<Ingredient>) :
     RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
+    private var quantity: Double = 1.0
 
     class ViewHolder(binding: ItemIngredientBinding) : RecyclerView.ViewHolder(binding.root) {
         val textView: TextView = binding.tvIngredient
         val quantity: TextView = binding.tvQuantity
-}
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         val binding =
             ItemIngredientBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return ViewHolder(binding)
     }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val ingredient: Ingredient = dataSet[position]
-
+        var ingredient: Ingredient = dataSet[position]
         holder.textView.text = ingredient.description
         holder.quantity.text = "${ingredient.quantity} ${ingredient.unitOfMeasure}"
+        var updateQuant = ingredient.quantity.toDouble() * quantity
+        if (updateQuant % 1 == 0.0) {
+            val updateQuantWithoutZero = updateQuant.toInt()
+            holder.quantity.text = "${updateQuantWithoutZero} ${ingredient.unitOfMeasure}"
+        } else {
+            holder.quantity.text = "${updateQuant} ${ingredient.unitOfMeasure}"
+        }
 
     }
 
     override fun getItemCount(): Int = dataSet.size
 
+    fun updateIngredients(progress: Double) {
+        quantity = progress
+        notifyDataSetChanged()
+    }
 
 }
--- app/src/main/java/com/example/recipeapp/Recipes/RecipeFragment.kt
+++ app/src/main/java/com/example/recipeapp/Recipes/RecipeFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,7 +20,7 @@ import com.google.android.material.divider.MaterialDividerItemDecoration
 
 class RecipeFragment : Fragment(R.layout.fragment_recipe) {
 
-
+    private lateinit var seekBar: SeekBar
     private var _binding: FragmentRecipeBinding? = null
     private val binding
         get() = _binding
@@ -34,6 +35,7 @@ class RecipeFragment : Fragment(R.layout.fragment_recipe) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        seekBar = (_binding?.sbFragmentRecipe ?: 0) as SeekBar
         val recipe: Recipe? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             requireArguments().getParcelable(ARG_RECIPE, Recipe::class.java)
         } else {
@@ -52,7 +54,6 @@ class RecipeFragment : Fragment(R.layout.fragment_recipe) {
             Log.d("Not found", "Image not found: ${recipe?.imageUrl}")
             null
         }
-
         if (recipe?.title == null) {
             recipe?.title == "я потерял ${recipe?.title}"
         }
@@ -66,6 +67,23 @@ class RecipeFragment : Fragment(R.layout.fragment_recipe) {
         val id: Int = recipeId?.id ?: 1
         val ingredientsAdapter =
             IngredientsAdapter(STUB.getRecipeById(id)?.ingredients ?: emptyList())
+        // не понимаю зачем выводить операции в класс IngredientsAdapter.
+        //  из за того что у нас там биндинги на элементы вывода на экран? получается 77 строка тоже идет в адаптер?
+        // val textView: TextView = binding.tvIngredient
+        //val quantity: TextView = binding.tvQuantity
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.tvFragmentRecipe.text = "Порции: $progress"
+                ingredientsAdapter.updateIngredients(progress.toDouble())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
         binding.rvIngredients.adapter = ingredientsAdapter
         val methodsAdapter = MethodsAdapther(STUB.getRecipeById(id)?.method ?: emptyList())
         binding.rvMethod.adapter = methodsAdapter
@@ -75,12 +93,18 @@ class RecipeFragment : Fragment(R.layout.fragment_recipe) {
             isLastItemDecorated = false
             dividerInsetStart = 24
             dividerInsetEnd = 24
-            dividerColor = ContextCompat.getColor(requireContext(), R.color.recipe_fragment_color)
+            dividerColor =
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.recipe_fragment_color
+                )
             dividerThickness = 6
         }
 
         binding.rvIngredients.addItemDecoration(divider)
         binding.rvMethod.addItemDecoration(divider)
+
+
     }
 
     override fun onDestroyView() {
