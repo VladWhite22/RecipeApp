8dcb29ec1b0203ac1f68ecb17c40aad49d078c4c
VladWhite22 <vladislav22971@mail.ru>
2025-07-05 02:45:30 +0300

    RA-40 Добавить кеширование для списка рецептов

--- app/src/main/java/com/example/recipeapp/ui/recipes/recipeList/RecipesListVIewModel.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipes/recipeList/RecipesListVIewModel.kt
@@ -5,11 +5,15 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.application
 import androidx.lifecycle.viewModelScope
 import com.example.recipeapp.data.RecipeRepository
+import com.example.recipeapp.data.local.DB
 import com.example.recipeapp.model.Recipe
 import com.example.recipeapp.model.RequestResult
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class RecipesListVIewModel(application: Application) : AndroidViewModel(application) {
     data class RecipeListUIState(
@@ -22,17 +26,31 @@ class RecipesListVIewModel(application: Application) : AndroidViewModel(applicat
     val recipeListState: LiveData<RecipeListUIState>
         get() = privateRecipeListState
 
-    fun loadRecipe(id: Int) {
+    fun loadRecipe(categoryId: Int) {
         viewModelScope.launch {
-            val result = recipeRepository.getRecipesByCategoryId(id)
+            val result = recipeRepository.getRecipesByCategoryId(categoryId)
 
             when (result) {
                 is RequestResult.Success<List<Recipe>> -> {
+                    withContext(Dispatchers.IO) {
+                        val db = (application as DB).recipeDb
+                        db.recipesDao().insertRecipe(result.data)
+                    }
                     privateRecipeListState.value = RecipeListUIState(
                         recipeList = result.data
                     )
                 }
-                is RequestResult.Error -> Log.d("!!", "loadRecipe $result")
+
+                is RequestResult.Error -> {
+                    Log.d("!!", "loadRecipe $result")
+                    val recipeFromDb = withContext(Dispatchers.IO) {
+                        (application as DB).recipeDb.recipesDao().getRecipesByCategoryDivision(categoryId)
+                    }
+                    privateRecipeListState.value = RecipeListUIState(
+                        recipeList = recipeFromDb
+                    )
+
+                }
             }
         }
     }
